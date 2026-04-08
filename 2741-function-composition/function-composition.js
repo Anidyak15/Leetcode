ba74aa57296a4f5cf27dcf2d030751f853de5fcf
Anidya Karunakaran <anidya1521@gmail.com>
2026-04-09 00:10:09 +0530

    Time: 57 ms (47.09%) | Memory: 55.3 MB (33.52%) - LeetSync

---
+++ 2741-function-composition/function-composition.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    return function(x) {
+        // We start with x as the initial value
+        // We apply functions from right-to-left
+        return functions.reduceRight((acc, fn) => {
+            return fn(acc);
+        }, x);
+    };
+};
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
