01b169343a81281e91fe8c61a5bfcaacccec7095
Anidya Karunakaran <anidya1521@gmail.com>
2026-04-22 00:20:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (70.87%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+void preorder(struct TreeNode* root, int* result, int* index) {
+    if (root == NULL) return;
+
+    result[*index] = root->val;   
+    (*index)++;
+
+    preorder(root->left, result, index); 
+    preorder(root->right, result, index);
+}
+
+int* preorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* result = (int*)malloc(100 * sizeof(int)); 
+    *returnSize = 0;
+
+    preorder(root, result, returnSize);
+
+    return result;
+}
