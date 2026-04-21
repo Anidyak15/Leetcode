e784321f8a5ca8a0993b4fd3d148ae8d0fccf93c
Anidya Karunakaran <anidya1521@gmail.com>
2026-04-22 00:17:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (48.04%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.c
+++ 125-valid-palindrome/valid-palindrome.c
@@ -1,22 +1,36 @@
-#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
 
-bool isPalindrome(char* s) {
+// check alphanumeric
+int isAlphaNum(char c) {
+    if ((c >= 'a' && c <= 'z') ||
+        (c >= 'A' && c <= 'Z') ||
+        (c >= '0' && c <= '9'))
+        return 1;
+    return 0;
+}
+
+// convert to lowercase
+char toLowerCase(char c) {
+    if (c >= 'A' && c <= 'Z')
+        return c + 32;
+    return c;
+}
+
+int isPalindrome(char* s) {
     int left = 0;
     int right = strlen(s) - 1;
-    
+
     while (left < right) {
-        
-        while (left < right && !isalnum(s[left])) left++;
-        while (left < right && !isalnum(s[right])) right--;
-        
-        
-        if (tolower(s[left]) != tolower(s[right])) {
-            return false;
-        }
-        
+        while (left < right && !isAlphaNum(s[left])) left++;
+        while (left < right && !isAlphaNum(s[right])) right--;
+
+        if (toLowerCase(s[left]) != toLowerCase(s[right]))
+            return 0;
+
         left++;
         right--;
     }
-    
-    return true;
+
+    return 1;
 }
